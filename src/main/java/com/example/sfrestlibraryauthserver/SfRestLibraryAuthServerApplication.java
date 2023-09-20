8f64f9dc25d80be2f08f6c432cdc01d7d5e6563b
Gustavo de Paula <depaula.gustavo@outlook.com>
2023-09-20 11:15:18 -0300

    Added dependencies and confguration to the authentication server

--- src/main/java/com/example/sfrestlibraryauthserver/SfRestLibraryAuthServerApplication.java
+++ src/main/java/com/example/sfrestlibraryauthserver/SfRestLibraryAuthServerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SfRestLibraryAuthServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SfRestLibraryAuthServerApplication.class, args);
 	}
-
 }
