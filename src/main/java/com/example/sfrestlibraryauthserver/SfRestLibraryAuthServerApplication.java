fb537e27648f0b68caf393d338c022da4df8bda8
Gustavo de Paula <depaula.gustavo@outlook.com>
2023-09-20 10:22:03 -0300

    Initial commit

---
+++ src/main/java/com/example/sfrestlibraryauthserver/SfRestLibraryAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfrestlibraryauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfRestLibraryAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfRestLibraryAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sfrestlibraryauthserver/SfRestLibraryAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfrestlibraryauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfRestLibraryAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
